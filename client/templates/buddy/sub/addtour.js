02448a6a27d74e7b9573e2f2edf25a7c17fa42f7
Admiral Ato <admiralato@gmail.com>
2017-12-13 19:12:57 +0800

    updates

--- client/templates/buddy/sub/addtour.js
+++ client/templates/buddy/sub/addtour.js
@@ -1,6 +1,13 @@
+noOfGuests = 1;
+
 Template.addtour.onCreated(function() {
-    document.title = "Yaplin - Add Tours";
+    document.title = "Yaplin - Create Tour";
 });
+
+Template.addtour.rendered = function() {
+    $('#start_01').datetimepicker();
+    $('#end_01').datetimepicker();
+};
 
 Template.addtour.events({
     'submit form': function(event){
@@ -9,8 +16,15 @@ Template.addtour.events({
         var target = event.target;
         var data = {
             title: target.title.value,
-            location: target.location.value
-        }
+            location: target.location.value,
+            guests: noOfGuests,
+            price: target.price.value,
+            summary: target.summary.value,
+            experience: target.experience.value,
+            exp_expectation: target.exp_expectation.value,
+            provision: target.provision.value,
+            prov_expectation: target.prov_expectation.value
+        };
 
         Meteor.call('CreateTour', data, function(error, response){
             
@@ -22,5 +36,25 @@ Template.addtour.events({
             }
         });
         return false;
+    },
+
+    "change #noOfGuests": function(event, template) {
+        var participant = $(event.currentTarget).val();
+        console.log("participant: " + participant);
+        noOfGuests = participant;
+    },
+
+    "click #btnAddSched": function(event) {
+        alert('Add schedule!');
+    },
+
+    "click #btnRemoveSched": function(event) {
+        alert('Remove schedule!');
     }
-})
+});
+
+Template.addtour.helpers({
+    participants: function(){
+        return [1, 2, 3, 4, 5];
+    }
+});
