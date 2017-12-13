4d7da0bc240a59dde18ce18a416ace0670c4b8bb
Admiral Ato <admiralato@gmail.com>
2017-12-13 21:16:03 +0800

    updates

--- client/templates/buddy/sub/addtour.js
+++ client/templates/buddy/sub/addtour.js
@@ -2,6 +2,12 @@ noOfGuests = 1;
 
 Template.addtour.onCreated(function() {
     document.title = "Yaplin - Create Tour";
+    Session.set('schedules', []);
+    
+    var schedules = Session.get('schedules');
+    var uniqid = Math.floor(Math.random() * 100000);
+    schedules.push({uniqid: uniqid, from: "", to: ""});
+    Session.set('schedules', schedules);
 });
 
 Template.addtour.rendered = function() {
@@ -44,17 +50,55 @@ Template.addtour.events({
         noOfGuests = participant;
     },
 
-    "click #btnAddSched": function(event) {
-        alert('Add schedule!');
+    "click .add-sched": function(event) {
+        var schedules = Session.get('schedules');
+        var uniqid = Math.floor(Math.random() * 100000);
+        schedules.push({uniqid: uniqid, from: "", to: ""});
+        Session.set('schedules', schedules);
+
+        $('#schedules').append('<tr id="row-' + uniqid +'"><td><div class="form-group"><div class="input-group date start" id="start-' + uniqid + '"><input type="text" class="form-control form-control-lg" placeholder="From"/> \
+                    <span class="input-group-addon"> \
+                        <span class="glyphicon glyphicon-calendar"></span> \
+                    </span> \
+                </div> \
+            </div> \
+        </td> \
+        <td> \
+            <div class="form-group"> \
+                <div class="input-group date end" id="end-' + uniqid + '"> \
+                    <input type="text" class="form-control form-control-lg" placeholder="To"/> \
+                    <span class="input-group-addon"> \
+                        <span class="glyphicon glyphicon-calendar"></span> \
+                    </span> \
+                </div> \
+            </div> \
+        </td> \
+        <td><button type="button" class="btn btn-success add-sched" data-toggle="tooltip" title="Add schedule"> \
+                + \
+            </button> <button name="' + uniqid +'" type="button" class="btn btn-danger remove-sched" data-toggle="tooltip" title="Remove schedule"> \
+            -</button></td> \
+        </tr>');
+        $('#start-'+uniqid).datetimepicker();
+        $('#end-'+uniqid).datetimepicker();
+        console.log(Session.get('schedules'));
     },
 
-    "click #btnRemoveSched": function(event) {
-        alert('Remove schedule!');
+    "click .remove-sched": function(event) {
+        var buttonName = event.target.name;
+        // TODO: Remove the deleted row from the Session
+        // console.log(Session.key[uniqid]);
+
+        // delete Session.value[buttonName];
+        $('table#schedules tr#row-' + buttonName).remove();
     }
 });
 
 Template.addtour.helpers({
     participants: function(){
         return [1, 2, 3, 4, 5];
+    },
+
+    schedules: function() {
+        return Session.get('schedules');
     }
 });
