7085b94e7f734061f34d543564d120c0fa3bb3c3
Jervine <angzyjervine@gmail.com>
2018-01-27 21:43:57 +0800

    1) Oh My

--- server/paypal_config.js
+++ server/paypal_config.js
@@ -1,6 +1,5 @@
 Meteor.Paypal.config({
     'host': 'api.sandbox.paypal.com',
     'port': '',
-    'client_id': 'AdFS7Yqx3uX106wZ-MRF2cktNcWHLXMp7sMxkHAP73aOiOn7usKY9_VkDLY2hPt_tpqUQ279V63VTSK4',
-    'client_secret': 'EL9egAPvQjfg27OtuvhBxaPX5sfzf0FgC6Za_DKsR3sZTxeUDw2SpC7zUC7XwlCgTwB7AxXfsI4NGn4z'
+    
   });
